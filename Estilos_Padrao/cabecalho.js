017e86f06f3f2d6f7103bc1419b53e500598959c
andreasseituno <andre.mendes.germinare@gmail.com>
2024-01-14 16:07:05 -0300

    cabecalho dinamico

--- Estilos_Padrao/cabecalho.js
+++ Estilos_Padrao/cabecalho.js
@@ -1,9 +1,12 @@
+// Gerando Cabecalho
+const header = document.createElement("header");
+const aside = document.createElement("aside");
+const cabecalhoFixo = document.getElementById("cabecalhoFixo");
+
+criaCabecalho()
+
 // Listas
 const botoes = document.querySelectorAll(".containerLogo button");
-
-// Elementos
-const cabecalho = document.getElementById("principal");
-const lateral = document.querySelector("aside");
 
 // Variáveis
 var fechado = true;
@@ -11,57 +14,62 @@ var scrollPos = 0;
 
 // Eventos
 window.addEventListener("scroll", () => {
-  if (document.body.getBoundingClientRect().top > scrollPos ||
-    window.scrollY <= 300)
+  if (
+    document.body.getBoundingClientRect().top > scrollPos ||
+    window.scrollY <= 300
+  )
     apareceCabecalho();
-
-  else 
-  escondeCabecalho();
+  else escondeCabecalho();
 
   scrollPos = document.body.getBoundingClientRect().top;
 });
 
 window.addEventListener("click", (e) => {
-  var clicouForaAside = !lateral.contains(e.target);
-  var clicouForaCabecalho = !cabecalho.contains(e.target);
+  var clicouForaAside = !aside.contains(e.target);
+  var clicouForaCabecalho = !header.contains(e.target);
 
-  if (clicouForaAside && clicouForaCabecalho && !fechado) 
-  fechaAside();
+  if (clicouForaAside && clicouForaCabecalho && !fechado) fechaAside();
 });
 
 for (let i = 0; i < botoes.length; i++) {
   botoes[i].addEventListener("click", () => {
-    if (fechado) 
-    abreAside();
-
-    else 
-    fechaAside();
+    if (fechado) abreAside();
+    else fechaAside();
   });
 }
 
 // Métodos
+function criaCabecalho() {
+  header.id = "principal";
+  header.innerHTML = window.headerDinamico;
+  aside.innerHTML = window.cabecalhoDinamico;
+
+  cabecalhoFixo.appendChild(header);
+  cabecalhoFixo.appendChild(aside);
+}
+
 function apareceCabecalho() {
-  cabecalho.style.opacity = "1";
-  cabecalho.style.scale = "1";
-  cabecalho.style.transition = "opacity .35s ease .65s";
+  header.style.opacity = "1";
+  header.style.scale = "1";
+  header.style.transition = "opacity .35s ease .65s";
 }
 
 function escondeCabecalho() {
-  cabecalho.style.opacity = "0";
-  cabecalho.style.scale = "0";
-  cabecalho.style.transition = "opacity .35s ease .4s, scale 0s ease .75s";
+  header.style.opacity = "0";
+  header.style.scale = "0";
+  header.style.transition = "opacity .35s ease .4s, scale 0s ease .75s";
 }
 
 function abreAside() {
-  lateral.style.marginLeft = "0";
-  lateral.style.visibility = "visible";
-  lateral.style.transition = "margin .25s ease 0s";
+  aside.style.marginLeft = "0";
+  aside.style.visibility = "visible";
+  aside.style.transition = "margin .25s ease 0s";
   fechado = false;
 }
 
 function fechaAside() {
-  lateral.style.marginLeft = "-400px";
-  lateral.style.visibility = "hidden";
-  lateral.style.transition = "margin .25s ease 0s, visibility 0s ease .2501s";
+  aside.style.marginLeft = "-400px";
+  aside.style.visibility = "hidden";
+  aside.style.transition = "margin .25s ease 0s, visibility 0s ease .2501s";
   fechado = true;
 }
